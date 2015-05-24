c80b5d46613796a4263945861e9c8ee519077414
Lufteluke <ratchetfan92@gmail.com>
2015-05-24 19:11:22 +0200

    Dangerous object

---
+++ Assets/Scripts/World/DangerousObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DangerousObject : MonoBehaviour
+{	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Player 1" || other.gameObject.tag == "Player 2")
+		{
+			other.gameObject.GetComponent<Fighter>().Kill();
+		}
+	}
+}
